29504876ec00671c49b0483527f4733a35cfac23
Mart van der Zalm <martvanderzalm@gmail.com>
2023-10-27 02:07:41 +0200

    Bootloader

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,62 +1,75 @@
-BITS 32
+; *********************************************************
+;	KnightOS
+; 	
+;	Operating System build by Mart van der Zalm
+; *********************************************************
 
-jmp short bootloader_start                                                                         		 	 ; Jump to the start of the bootloader
-nop                                                                                                		     ; The reason for nop is to jump over the disk information
+bits 16 																									 ; We are for now in 16 bit real mode
 
+org 0x7c00																						   			 	 ; We are loaded by BIOS at 0x7C00
 
-; -------------------------------------------------------------------
-; SECTOR 0: BPB (Bios Parameter Block)
-; -------------------------------------------------------------------
-OEMLabel                     db                  "KNIGHTOS"                                                  ; OEM name
-BytesPerSector               dw                  512                                                         ; Bytes per sector
-SectorsPerCluster            db                  8                                                           ; Sectors per cluster
-ReservedSectors              dw                  32                                                          ; Reserved sectors for boot record
-NumberOfFats                 db                  2                                                           ; Number of FATs
-RootDirEntries               dw                  0                                                           ; For FAT32, set to 0
-TotalSectors16               dw                  0                                                           ; For FAT32, set to 0
-Media                        db                  0xF8                                                        ; Media descriptor (fixed disk)
-SectorsPerFat16              dw                  0                                                           ; For FAT32, set to 0
-SectorsPerTrack              dw                  63                                                          ; Sectors per track
-Heads                        dw                  255                                                         ; Number of heads
-HiddenSectors                dd                  0                                                           ; Number of hidden sectors
-TotalSectors32               dd                  0                                                           ; Total sectors (use 32-bit value for FAT32)
+start: jmp loader																							 ; Jump over BPB block
 
-; -------------------------------------------------------------------
-; SECTOR 1: EBR (Extended Boot Record)
-; -------------------------------------------------------------------
-SectorsPerFat32              dd                  0                                                           ; Sectors per FAT (set to 0 for FAT32)
-Flags                        dw                  0                                                           ; Flags (typically 0)
-FileSystemVersion            dd                  0                                                           ; File system version (typically 0)
-RootCluster                  dd                  2                                                           ; First cluster of the root directory (typically 2)
-FileSystemInfoSector         dw                  1                                                           ; File system information sector
-BackupBootSector             dw                  6                                                           ; Backup boot sector
-Reserved                     db                  12                                                          ; Reserved bytes
-DriveNumber                  db                  0x80                                                        ; Drive number (typically 0x80)
-WinNtFlags                   db                  0                                                           ; Windows NT flags (typically 0)
-Signature                    db                  0x29                                                        ; Signature (0x28 and 0x29 are common)
-VolumeID                     dd                  0xAB29F1C7                                                  ; A unique volume ID
-VolumeLabel                  db                  "MYFAT32"                                                   ; Volume label
-FileSystem                   db                  "FAT32   "                                                  ; File system type
+; *********************************************************
+;	BPB (Bios Parameter Block)
+; *********************************************************
 
-; -------------------------------------------------------------------
-; SECTOR 2: FSinfo Structure (File System Information)
-; -----------------------------------------------------------
-FSInfoLeadSignature           dd                 0x41615252                                                  ; FSINFO lead signature ("RRaA")
-FSInfoStructureSignature      dd                 0x61417272                                                  ; FSINFO structure signature ("rrAa")
-FreeClusterCount              dd                 0xFFFFFFFF                                                  ; Number of free clusters (set to a placeholder value)
-MostRecentCluster             dd                 0xFFFFFFFF                                                  ; Most recently allocated cluster (set to a placeholder value)
-Reserved                      dd                 0x00000000                                                  ; Reserved (typically set to 0x00000000)
-FSInfoSignature               dd                 0xAA550000                                                  ; FSinfo sector signature (for the end of the sector)
+OEMLabel                    DB "KNIGHTOS"                                        							 ; OEM (Original Equipment Manufacturer)
+bpbBytesPerSector:			DW 512
+bpbSectorsPerCluster:		DB 1
+bpbReservedSectors:			DW 1
+bpbNumberOfFATs:			DB 2
+bpbRootEntries:				DW 224
+bpbTotalSectors:			DW 2880
+bpbMedia:					DB 0xF0
+bpbSectorsPerFAT:			DW 9
+bpbSectorsPerTrack:			DW 18
+bpbHeadsPerCylinder:		DW 2
+bpbHiddenSectors:			DD 0
+bpbTotalSectorsBig:			DD 0
+bsDriveNumber:				DB 0
+bsUnused:					DB 0
+bsExtBootSignature:			DB 0x29
+bsSerialNumber:				DD 0xAB29F1C7
+bsVolumeLabel:              DB "KNIGHT OS  "
+bsFileSystem:               DB "FAT12   "
 
+msg	DB	"Welcome to My Operating System!", 0		; the string to print
 
-; -------------------------------------------------------------------
-; Main bootloader code
-; -------------------------------------------------------------------
-bootloader_start:
+; *********************************************************
+;	Prints a string
+; 	DS=>SI: 0 terminated string
+; *********************************************************
+Print:
+	lodsb
+	or al, al
+	jz PrintDone
+	mov ah, 0eh
+	int 10h
+	jmp Print
 
+PrintDone:
+	ret 
 
+; *********************************************************
+;	Bootloader Entry Point
+; *********************************************************
 
-; -------------------------------------------------------------------
-; END OF BOOTLOADER
-; -------------------------------------------------------------------
-dw 0AA55h                                                                                                    ; Boot signature
+loader:
+
+	xor	ax, ax																								 ; Setup segments to insure they are 0. Remember that
+	mov	ds, ax																								 ; we have ORG 0x7c00. This means all addresses are based
+	mov	es, ax																																															
+
+	mov	si, msg																								 ; our message to print
+	call Print																							 	 ; call our print function
+
+	xor	ax, ax																								 ; clear ax
+	int	0x12																								 ; get the amount of KB from the BIOS
+
+	cli																										 ; Clear all Interrupts
+	hlt																										 ; halt the system
+	
+times 510 - ($-$$) DB 0																						 ; We have to be 512 bytes. Clear the rest of the bytes with 0
+
+DW 0xAA55																								     ; Boot Signiture
