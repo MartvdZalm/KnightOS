cefa47681350087fb09767ae2ea3be2d00d45121
Mart van der Zalm <martvanderzalm@gmail.com>
2023-11-25 00:36:18 +0100

    Kernel

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "gdt.h"
 #include "interrupts.h"
+#include "keyboard.h"
 
 void printf(char* str)
 {
@@ -59,6 +60,9 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
 
 	GlobalDescriptorTable gdt;
 	InterruptManager interrupts(&gdt);
+
+	KeyboardDriver keyboard(&interrupts);
+
 	interrupts.Activate();
 
 	while (1);
